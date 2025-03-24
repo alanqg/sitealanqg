9367879ce7dc908ec162226c93ee89723197200d
Alan Queirós de Goes <alanqg@gmail.com>
2025-03-24 13:49:46 -0300

    Principais páginas criadas

--- js/customer.js
+++
@@ -1,9 +0,0 @@
-/*Projeto desenvolvido por AlanQg */
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
